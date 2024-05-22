d261bd2b1dc73756e01c7dd4a764906a14c5db82
Joohy2 <90262714+Joohy2@users.noreply.github.com>
2024-05-22 09:54:56 +0900

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -9,12 +9,6 @@ KEY="$GITHUB_SHA.zip"  # 이 변수는 CI/CD 환경에서 설정되어 있어야
 
 # AWS CodeDeploy를 이용한 배포 생성
 echo "Creating deployment for application $APPLICATION_NAME..."
-aws deploy create-deployment \
-    --application-name "$APPLICATION_NAME" \
-    --deployment-group-name "$DEPLOYMENT_GROUP_NAME" \
-    --s3-location bucket="$S3_BUCKET",bundleType="$BUNDLE_TYPE",key="$KEY" || {
-        echo "Error: Deployment failed."
-        exit 1
-    }
+aws deploy create-deployment --application-name "joo-deploy-cicd" --deployment-group-name "joo-deploy-group" --s3-location bucket="cicd-joo-terraform",bundleType=zip,key="example.zip"
 
 echo "Deployment initiated successfully."
